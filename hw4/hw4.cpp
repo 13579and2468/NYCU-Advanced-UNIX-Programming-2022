13ac4f4cfc29d80f8d3247866198e818c1c186d2
a13579and2468 <a13579and2468@a13579and2468-X555LD>
2022-05-30 19:02:34 +0800

    control flow complete

--- hw4/hw4.cpp
+++ hw4/hw4.cpp
@@ -15,32 +15,56 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
 void help();
-void load();
+void load(string);
 void run();
 void getregs();
 void get();
 void dump();
+void setbreak();
+void c();
+void list();
+void si();
+void vmmap();
+unsigned long get_addr();
+void reset_all_breakpoint();
+void si_and_restore_break();
 
+typedef struct breakpoint
+{
+    unsigned char savecode;
+    unsigned long addr;
+} breakpoint_t;
+
+static unsigned long idx = 1;
 static string state;
 static pid_t child;
 static int child_status;
+static map<unsigned long, breakpoint_t> breakpoints;
+static string process_name;
+static unsigned long restore_idx = 0;
+static stringstream global_cmd;
 int main(int argc, char *argv[])
 {
     state = "not loaded";
     string cmd;
-    while (cerr << "sdb > " && cin >> cmd )
+    while (cerr << "sdb > ")
     {
+        getline(cin, cmd);
+        global_cmd = stringstream(cmd);
+        global_cmd >> cmd;
         if (cmd == "help" || cmd == "h")
         {
             help();
         }
         else if (cmd == "load")
         {
-            load();
+            load("");
         }
         else if(cmd == "start")
         {
@@ -49,6 +73,10 @@ int main(int argc, char *argv[])
         else if (cmd == "run" || cmd == "r")
         {
             run();
+        }
+        else if (cmd == "c" || cmd == "cont")
+        {
+            c();
         }
         else if (cmd == "getregs")
         {
@@ -62,26 +90,114 @@ int main(int argc, char *argv[])
         {
             dump();
         }
+        else if(cmd == "exit" || cmd == "q")
+        {
+            exit(0);
+        }
+        else if(cmd == "break" || cmd == "b")
+        {
+            setbreak();
+        }
+        else if(cmd == "list" || cmd == "l")
+        {
+            list();
+        }
+        else if (cmd == "si")
+        {
+            si();
+        }else if(cmd == "vmmap" || cmd == "m")
+        {
+            vmmap();
+        }
     }
 
     return 0;
 }
 
-void dump()
+void vmmap()
 {
-    string addr_str;
-    unsigned long addr;
-    cin >> addr_str;
-    stringstream ss;
-    ss << addr_str;
-    if (addr_str.substr(0, 2) == "0x")
+    string filename = "/proc/" + to_string(child) + "/maps";
+    string line;
+    ifstream myfile(filename);
+    if (myfile.is_open())
     {
-        ss >> hex >> addr;
+        while (getline(myfile, line))
+        {
+            stringstream ss(line);
+            string address;
+            ss >> address;
+            cerr << setfill('0') << setw(16) <<address.substr(0, address.find('-'));
+            cerr << '-';
+            address.erase(0, address.find('-') + 1);
+            cerr << setfill('0') << setw(16) << address;
+            string rwx;
+            ss >> rwx;
+            cerr << " " << rwx.substr(0, 3);
+            unsigned long offset;
+            ss >> hex >> offset;
+            cerr << " " << hex << offset;
+            string path;
+            string ignore;
+            ss >> ignore >> ignore >> path;
+            cerr << " " << path << endl;
+        }
+        myfile.close();
+    }
+}
+
+void si()
+{
+    struct user_regs_struct regs;
+    ptrace(PTRACE_GETREGS, child, 0, &regs);
+    unsigned long word;
+    word = ptrace(PTRACE_PEEKDATA, child, regs.rip, 0);
+    if((word & 0xff) == 0xcc)
+    {
+        for(auto b : breakpoints)
+        {
+            if(b.second.addr = regs.rip)
+            {
+                restore_idx = b.first;
+                break;
+            }
+        }
+        si_and_restore_break();
     }
     else
     {
-        ss >> addr;
+        do
+        {
+            ptrace(PTRACE_SINGLESTEP, child, 0, 0);
+            waitpid(child, &child_status, 0);
+        } while (!WIFSTOPPED(child_status));
     }
+}
+
+void list()
+{
+    for(auto b : breakpoints)
+    {
+        cerr << b.first << ":   " << hex << b.second.addr << endl;
+    }
+}
+
+void setbreak()
+{
+    unsigned long addr = get_addr();
+    if (addr == 0xffffffffffffffff)return;
+
+    unsigned long word;
+    word = ptrace(PTRACE_PEEKDATA, child, addr, 0);
+    if (ptrace(PTRACE_POKETEXT, child, addr, (word & 0xffffffffffffff00) | 0xcc) != 0)
+        errquit("ptrace(POKETEXT)");
+
+    breakpoints.insert({idx++,{(unsigned char)(word & 0xff), addr}});
+}
+
+void dump()
+{
+    unsigned long addr = get_addr();
+    if (addr == 0xffffffffffffffff)return;
 
     for (int i = 0; i < 5;i++)
     {
@@ -125,7 +241,7 @@ void dump()
 void get()
 {
     string reg;
-    cin >> reg;
+    global_cmd >> reg;
     struct user_regs_struct regs;
     ptrace(PTRACE_GETREGS, child, 0, &regs);
     if(reg == "rax")cerr << "rax = " << regs.rax << hex <<" (0x"<< regs.rax <<")"<< endl;
@@ -178,21 +294,69 @@ void run()
     {
         cerr << "** process is already running!\n";
     }
-    ptrace(PTRACE_CONT,child,0,0);
     state = "running";
+    c();
 }
 
-void load()
+void c()
 {
-    string argv;
-    cin >> argv;
+    if (restore_idx)si_and_restore_break();
+
+    struct user_regs_struct regs;
+    ptrace(PTRACE_CONT, child, 0, 0);
+    while (waitpid(child, &child_status, 0) > 0)
+    {
+        if (WIFEXITED(child_status))
+        {
+            cerr << "** process exit!\n";
+            load(process_name);
+            break;
+        }
+
+        if (!WIFSTOPPED(child_status))
+            continue;
+
+        struct user_regs_struct regs;
+        if (ptrace(PTRACE_GETREGS, child, 0, &regs) != 0)
+            errquit("ptrace(GETREGS)");
+
+        int idx = 0;
+        for (auto b : breakpoints)
+        {
+            if(b.second.addr == regs.rip - 1) 
+            {
+                idx = b.first;
+                break;
+            }
+        }
+
+        if(idx)
+        {
+            regs.rip = regs.rip - 1;
+            restore_idx = idx;
+            cerr << "** breakpoint @      " << hex << regs.rip << endl;
+
+            if (ptrace(PTRACE_SETREGS, child, 0, &regs) != 0)
+                errquit("ptrace(SETREGS)");
+        }
+
+        break;
+    }
+}
+
+void load(string proc_name)
+{
+    if(proc_name == "")
+    {
+        global_cmd >> process_name;
+    }
     if ((child = fork()) < 0)
         errquit("fork");
     if (child == 0)
     {
         if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0)
             errquit("ptrace@child");
-        execlp(argv.c_str(), argv.c_str(), NULL);
+        execlp(process_name.c_str(), process_name.c_str(), NULL);
         errquit("execlp");
     }
 
@@ -203,10 +367,11 @@ void load()
         ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL);
         struct user_regs_struct regs;
         ptrace(PTRACE_GETREGS, child, 0, &regs);
-        cerr << "** program '" << argv << "' loaded. entry point " << "0x" << hex << regs.rip << endl;
+        cerr << "** program '" << process_name << "' loaded. entry point "<< "0x" << hex << regs.rip << endl;
+        process_name = process_name;
         state = "loaded";
+        reset_all_breakpoint();
     }
-
 }
 
 void help()
@@ -228,4 +393,58 @@ void help()
     - si: step into instruction
     - start: start the program and stop at the first instruction
 )";
+}
+
+unsigned long get_addr()
+{
+    string addr_str = "";
+    unsigned long addr;
+    global_cmd >> addr_str;
+    if(addr_str == "")
+    {
+        cerr << "**no addr is given\n";
+        return 0xffffffffffffffff;
+    }
+
+    stringstream ss;
+    ss << addr_str;
+    if (addr_str.substr(0, 2) == "0x")
+    {
+        ss >> hex >> addr;
+    }
+    else
+    {
+        ss >> addr;
+    }
+
+    return addr;
+}
+
+void reset_all_breakpoint()
+{
+    for (auto b : breakpoints)
+    {
+        unsigned long word;
+        word = ptrace(PTRACE_PEEKDATA, child, b.second.addr, 0);
+        if (ptrace(PTRACE_POKETEXT, child, b.second.addr, (word & 0xffffffffffffff00) | 0xcc) != 0)
+            errquit("ptrace(POKETEXT)");
+    }
+    restore_idx = 0;
+}
+
+void si_and_restore_break()
+{
+    /* restore ins */
+    unsigned long word = ptrace(PTRACE_PEEKDATA, child, breakpoints[restore_idx].addr, 0);
+    if (ptrace(PTRACE_POKETEXT, child, breakpoints[restore_idx].addr, (word & 0xffffffffffffff00) | breakpoints[restore_idx].savecode) != 0)
+        errquit("ptrace(POKETEXT)");
+
+    
+    do{
+        ptrace(PTRACE_SINGLESTEP,child,0,0);
+        waitpid(child, &child_status, 0);
+    } while (!WIFSTOPPED(child_status));
+
+    /* restore break point */
+    reset_all_breakpoint();
 }
---
+++ hw4/test.cpp
@@ -0,0 +1,14 @@
+#include <signal.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long gg;
+    cin >> hex >> gg;
+    cout << hex << gg;
+
+    return 0;
+}
